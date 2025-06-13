55d9ae4c9c1289516580ebc6ac489dd7d76a76e7
KomalSWaghule <komalwaghule05@gmail.com>
2025-06-13 23:55:26 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');  // Adjust the path accordingly
+const User = require('../models/User');  
 const Post = require('../models/Post');
 
 const { getUserProfile, followUser, unfollowUser } = require('../controllers/userController');
