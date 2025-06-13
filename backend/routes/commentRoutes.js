7c7dd8f07bd70d0b1a195d6dfb143693108aeca0
KomalSWaghule <komalwaghule05@gmail.com>
2025-06-13 23:55:13 +0530

    Update commentRoutes.js

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -4,13 +4,11 @@ const router = express.Router();
 const { addComment, getComments, updateComment } = require('../controllers/commentController');
 const auth = require('../middleware/authMiddleware');
 
-// Add a comment to a post
+
 router.post('/:postId', auth, addComment);
 
-// Get comments for a post
 router.get('/:postId', auth, getComments);
 
-// ✅ Edit a comment
 router.put('/:commentId', auth, updateComment);
 
 module.exports = router;
