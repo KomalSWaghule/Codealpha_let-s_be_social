5cc035a27d5ad58b255dd842ea59194330e87c61
KomalSWaghule <komalwaghule05@gmail.com>
2025-06-13 23:59:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,18 +26,18 @@ mongoose
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-// API Routes
+
 app.use("/auth", authRoutes);         
 app.use("/posts", postRoutes);        
 app.use("/users", userRoutes);        
 app.use("/comments", commentRoutes);  
 
-// Serve HTML page
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "auth.html"));
 });
 
-// Start the server
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
