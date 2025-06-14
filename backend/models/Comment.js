d4d5560477f0e33fc1eac60ff415cc6c84b7fa52
KomalSWaghule <komalwaghule05@gmail.com>
2025-06-14 15:39:11 +0530

    Update Comment.js

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -2,11 +2,11 @@
 
 const mongoose = require('mongoose');
 
-const CommentSchema = new mongoose.Schema({
+const Commentstructure = new mongoose.Schema({
   postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   text: String,
   createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Comment', CommentSchema);
+module.exports = mongoose.model('Comment', Commentstructure);
