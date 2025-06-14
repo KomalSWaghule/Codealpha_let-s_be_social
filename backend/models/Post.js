b0228e883c392b1419b6651d18cd8c2c9e8b060e
KomalSWaghule <komalwaghule05@gmail.com>
2025-06-14 15:39:40 +0530

    Update Post.js

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
+const Poststructure = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   content: { type: String, required: true },
   image: { type: String }, 
@@ -9,4 +9,4 @@ const PostSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Post", PostSchema);
+module.exports = mongoose.model("Post", Poststructure);
